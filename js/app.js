61b72185c4cb9be2e142ab3125657613a976d72b
Katie Allyn <kaallyn@gmail.com>
2018-09-02 02:07:04 -0500

    city height

--- js/app.js
+++ js/app.js
@@ -44,3 +44,14 @@ pauseButton.addEventListener("click", function() {
   }
 });
 
+$('body').on('click', '.btn', function(e){
+    e.preventDefault();
+    if ( $(this).hasClass('play') ) {
+        $(this).removeClass('play');
+        $(this).addClass('pause');
+    } else {
+        $(this).removeClass('pause');
+        $(this).addClass('play');
+    }
+});
+
